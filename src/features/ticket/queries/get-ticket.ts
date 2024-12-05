422dd8c5dd967ea283b59f6397de683611f8da8f
Amin Dibmehr <dibmehrm@gmail.com>
2024-12-05 13:52:44 +0330

    80. Database queries

--- src/features/ticket/queries/get-ticket.ts
+++ src/features/ticket/queries/get-ticket.ts
@@ -1,12 +1,10 @@
-import { initialTickets } from "@/data";
+import { prisma } from "@/lib/prisma";
 import { Ticket } from "../types";
 
-export async function getTicket(ticketId: string): Promise<Ticket | null> {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-
-  const ticket = initialTickets.find((ticket) => ticket.id === ticketId);
-
-  return new Promise((resolve) => {
-    resolve(ticket || null);
+export async function getTicket(ticketId: string) {
+  return await prisma.ticket.findUnique({
+    where: {
+      id: ticketId,
+    },
   });
 }
